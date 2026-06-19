c0ff4719e8e2e0be23df6abc1c4afdf10ba6ebe0
Talentedfalcon <82446314+Talentedfalcon@users.noreply.github.com>
2026-06-20 01:26:30 +0530

    image generation

---
+++ image_gen.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+
+int main(){
+    int image_width=256;
+    int image_height=256;
+
+    std::ofstream outFile("temp.ppm",std::ios::binary);
+
+    try{
+        if(!outFile){
+            std::cerr << "Error: Could not open file for writing" << std::endl;
+            return 1;
+        }
+        outFile << "P6\n" << image_width << " " << image_height << "\n255\n";
+
+        for(int y=0;y<image_height;y++){
+            for(int x=0;x<image_width;x++){
+                double r=double(x)/(image_width-1);
+                double g=double(y)/(image_height-1);
+                double b=0.0;
+
+                int ir=int(255*r);
+                int ig=int(255*g);
+                int ib=int(255*b);
+
+                outFile.put(ir);
+                outFile.put(ig);
+                outFile.put(ib);
+            }
+        }
+        outFile.close();
+        std::cout << "Image generated successfully as 'output.ppm'" << std::endl;
+    }
+    catch(...){
+        std::cout << "Caught an unhandled exception\n";
+    }
+
+    return 0;
+}
