1e80ebefa416cc738cff88029013ba8acd99ee48
Talentedfalcon <82446314+Talentedfalcon@users.noreply.github.com>
2026-06-26 08:50:51 +0530

    total internal reflection and hollow glass spheres

--- material.h
+++ material.h
@@ -55,8 +55,14 @@ class metal: public material{
 
 class dielectric: public material{
     private:
-        color albedo;
         double refractive_index;
+
+        static double reflectance(double cosine, double refractive_index){
+            //Schlick's approximation for reflectance
+            double r0=(1-refractive_index)/(1+refractive_index);
+            r0*=r0;
+            return r0+(1-r0)*std::pow((1-cosine),5);
+        }
 
     public:
         dielectric(double refractive_index){
@@ -66,10 +72,20 @@ class dielectric: public material{
         bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override{
             attenuation=color(1,1,1);
             double ri=rec.front_face?(1.0/refractive_index):refractive_index;
+            
+            vec3 unit_direction=unit_vector(r_in.direction());
+            double cos_theta=std::fmin(dot(-unit_direction,rec.normal),1.0);
+            double sin_theta=std::sqrt(1.0-cos_theta*cos_theta);
 
-            vec3 refracted=refract(unit_vector(r_in.direction()),rec.normal,ri);
+            vec3 out_direction;
 
-            scattered=ray(rec.p,refracted);
+            if(ri*sin_theta>1.0 || reflectance(cos_theta,ri)>random_double()){
+                out_direction=reflect(unit_direction,rec.normal);
+            }
+            else{
+                out_direction=refract(unit_direction,rec.normal,ri);
+            }
+            scattered=ray(rec.p,out_direction);
             return true;
         }
 };
--- ray_sphere.cpp
+++ ray_sphere.cpp
@@ -13,13 +13,15 @@ int main(){
     std::shared_ptr<lambertian> mat_diffuse=std::make_shared<lambertian>(color(0.5,0.1,0.8));
     std::shared_ptr<metal> mat_steel=std::make_shared<metal>(color(0.8,0.8,0.8),0.2);
     std::shared_ptr<metal> mat_gold=std::make_shared<metal>(color(0.8,0.6,0.2),0);
-    std::shared_ptr<dielectric> mat_glass=std::make_shared<dielectric>(1.5);
+    std::shared_ptr<dielectric> mat_glass=std::make_shared<dielectric>(1.5); //(refractive index of air)/(refractive index of water)
+    std::shared_ptr<dielectric> mat_bubble=std::make_shared<dielectric>(1/1.5); //(refractive index of air)/(refractive index of water)
 
-    world.add(std::make_shared<sphere>(point3(0,0,-2.0),0.5,mat_diffuse));
     world.add(std::make_shared<sphere>(point3(0,-100.5,-1),100,mat_ground));
+    world.add(std::make_shared<sphere>(point3(0,0,-2.0),0.5,mat_diffuse));
     world.add(std::make_shared<sphere>(point3(1,0,-1.5),0.5,mat_steel));
     world.add(std::make_shared<sphere>(point3(-1,0,-1.5),0.5,mat_gold));
-    world.add(std::make_shared<sphere>(point3(0.25,0,-1.0),0.3,mat_glass));
+    world.add(std::make_shared<sphere>(point3(0.25,0,-1.0),0.4,mat_glass));
+    world.add(std::make_shared<sphere>(point3(0.25,0,-1.0),0.3,mat_bubble));
 
     camera cam;
 
